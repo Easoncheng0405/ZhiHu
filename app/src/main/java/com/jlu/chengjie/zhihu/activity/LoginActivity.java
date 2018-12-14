1c6cc992dfef3e47a1ed6e58f35d5dd3d1064828
Easoncheng0405 <597021782@qq.com>
2018-12-15 00:36:38 +0800

    design login activity, add toasty and circleImageView dependencies.

---
+++ app/src/main/java/com/jlu/chengjie/zhihu/activity/LoginActivity.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright [2018] [chengjie.jlu@qq.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jlu.chengjie.zhihu.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.jlu.chengjie.zhihu.R;
+
+import es.dmoral.toasty.Toasty;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.login)
+    void login() {
+        Toasty.success(this, "您的验证码是: 123456 ", Toast.LENGTH_LONG, true).show();
+    }
+}
